26b4614fb361c840be8023eb25549c4aadfe4922
Julia Honda <jhonda@hawk.iit.edu>
2020-11-14 15:58:24 -0600

    Add some corrections to the second problem and some JavaScript to the third problem

--- pp-04/site.js
+++ pp-04/site.js
@@ -24,8 +24,7 @@ console.log('The value of x is', x, '-- it should be 5.');
   can accurately test your work.
 
   --with the function above enetering: window.x I got 5 now with what I have
-  below when I enter window.x I get "Uncaught ReferenceError: windox is not defined
-    <anonymous> debugger eval code:1"
+  below when I enter window.x I get "Undefined"
   not sure if that is how you correctly not pollute the global namespace
   just what I saw in examples of "not polluting the global namespace in JavaScript"
 */
@@ -37,14 +36,28 @@ console.log('The value of x is', x, '-- it should be 5.');
    return x;
   }
  }
-})();
-
 double(6);
 console.log('The value of x is', x, '-- it should be 5.');
-
+})();
 
 /*
   Step 3. Rewrite your corrected `double()` function from Step 2.
   so that non-number values passed into the function are handled
   in some reasonable way.
+  -- if else statment if number is enter do this else do this and say thats a non-number
+  keep running double and keep getting an error that it isnt defined
 */
+
+(function(){
+ var x = 5;
+ function double(num) {
+   if (typeof(num) = "number" ){
+     var x = num * 2;
+     return x;
+   } else{
+     return ('That was a non-number value try again.')
+   }
+  }
+double(6);
+console.log('The value of x is', x, '-- it should be 5.');
+})();
